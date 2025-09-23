dcf1b882d055c7009179891c7f3bc0fd60bfa202
00ashvar <00ashvargas@gmail.com>
2025-09-23 14:53:41 -0400

    Initial commit

---
+++ src/New_code.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class New_code {
+    public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+    int student_num = input.nextInt();
+    }
+}
