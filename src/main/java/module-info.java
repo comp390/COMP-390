6921ffd8a88de370aad240488a15581f1726563f
Alexandra Arbia <157141229+aarbia@users.noreply.github.com>
2025-10-30 15:09:24 -0400

    Revert back for Swing instead

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.ridesharingapp.comp390ridesharingapp {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens org.ridesharingapp.comp390ridesharingapp to javafx.fxml;
-    exports org.ridesharingapp.comp390ridesharingapp;
-}
